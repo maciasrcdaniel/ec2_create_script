2ad7bfc29f26fddac3bc8b9cabaf976d5d38e550
Mac <mac@Macs-MacBook-Pro.local>
2023-07-29 13:22:54 -0700

    Beta Test - 1

--- ec2create.sh
+++ ec2create.sh
@@ -7,6 +7,8 @@ echo "Before starting make sure you have a user that has access key permissions 
 echo "MAKE SURE THAT YOU ARE IN THE SAME FOLDER AS YOUR USER DATA SCRIPT IF APPLICABLE" 
 sleep 2
 
+echo "--------------------------------------------------------------------------" 
+
 # --------------------------------------------------------------------------
 # KEYPAIR BLOCK 
 # NOTES: FUNCTION VS HARDCODING PROCEDURES
@@ -15,40 +17,66 @@ sleep 2
 echo "Requirements: User with CLI access, Keypair, Security Group, User Data(Optional)"
 sleep 2 
 
+echo "--------------------------------------------------------------------------" 
+
 # AWS CLI - KEYPAIR CREATION COMMANDS
 echo "What would you like the keypair to be named? "
 read -r keypairname
 sleep 2
+
+echo "--------------------------------------------------------------------------" 
 
 aws ec2 create-key-pair --key-name $keypairname --query 'KeyMaterial' --output text > $keypairname.pem
 sleep 2
 echo "Your keypair has been created: $keypairname" 
 sleep 2
 
+echo "--------------------------------------------------------------------------" 
+
+# --------------------------------------------------------------------------
+
 # AWS CLI - SECURITY GROUP CREATION 
 echo "What would you like to name the security group: " 
 read -r sggroupname
 sleep 2
 
+echo "--------------------------------------------------------------------------" 
+
 echo "Enter a description for the security group: " 
 read -r sggroupdescription
 sleep 2
 
-sgidname=`aws ec2 create-security-group --group-name $sggroupname --description $sggroupdescription` --query 'GroupId' --output text`
+echo "--------------------------------------------------------------------------" 
+
+sgidname=`aws ec2 create-security-group --group-name $sggroupname --description $sggroupdescription --query 'GroupId' --output text`
 sleep 2 
 echo "Your security group has been created: $sgidname" 
 
-
-
-
-
-# KEYPAIR CREATION AWS CLI COMMANDS & VERIFY KEY PAIR NAMING CONVENTIONS ARE FOLLOWED
-
-# read -r keypairname
-
-# newkeypairname = aws ec2 create-key-pairs --key-name $keypairname
-# echo "Keypair created: $newkeypairname"
+echo "--------------------------------------------------------------------------" 
 
 # --------------------------------------------------------------------------
+
+# CREATE EC2 INSTANCE
+
+echo "Enter your AMI image id: ex. ami-04sc92034..." 
+read -r amiimageid
+sleep 2
+
+echo "--------------------------------------------------------------------------" 
+
+echo "Select your instance type: Ex. t2.micro" 
+read -r instancetypeid
+sleep 2
+
+echo "--------------------------------------------------------------------------" 
+
+aws ec2 run-instances --image-id $amiimageid --instance-type $instancetypeid --security-group-ids $sgidname --key-name $keypairname 
+
+# COMMAND BELOW INCLUDES ADDITIONAL FIELDS LIKE SUBNET AND USER DATA 
+# REPLACE THE COMMAND ABOVE WITH THE ONE BELOW
+
+: '
+aws ec2 run-instances --image-id ami-06ca3ca175f37dd66 --instance-type t2.micro --security-group-ids sg-01175d375658cc570 --subnet-id subnet-0742f5170e2f67db1 --key-name LUIT_KEYS --user-data file://user-data-subnet-id.txt
+'
 
 
